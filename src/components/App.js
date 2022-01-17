bc8881d174c706a5e2c204fed76232cc58eb84b7
Wen Xiao <wennstyle@gmail.com>
2022-01-16 23:04:15 -0500

    commit tree 84d4a6769396d14abdbbb8772fbb0ac752da6398

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,17 @@ import user from "../data/user";
 console.log(user);
 
 function App() {
+
+  // const homeInfo(user){
+  //   return <Home name = {user.name}
+  //>
+  // }
+
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name = {user.name} city ={user.city} color = {user.color}/>
+      <About bio = {user.bio} links = {user.links}/>
     </div>
   );
 }
